733e78cd3047eb395118dad1b33e9d142ab1c023
RohanAlief <rafarafa92653@gmail.com>
2025-09-21 08:21:03 +0700

    final feature

--- script/script.js
+++ script/script.js
@@ -2,14 +2,17 @@ const menuLst = document.getElementById('hamburger-menu');
 const menuToggle = document.getElementById('menu-toggle');
 const overlay = document.querySelector('.overlay');
 
-//menu toggle
+// Toggle menu and overlay
 menuToggle.addEventListener('click', () => {
-  menuLst.classList.toggle('active');
-  overlay.classList.toggle('active');
+    const isExpanded = menuToggle.getAttribute('aria-expanded') === 'true';
+    menuToggle.setAttribute('aria-expanded', !isExpanded);
+    menuLst.classList.toggle('active');
+    overlay.classList.toggle('active');
 });
 
-// overlay
+// Close menu when clicking overlay
 overlay.addEventListener('click', () => {
+    menuToggle.setAttribute('aria-expanded', 'false');
     menuLst.classList.remove('active');
     overlay.classList.remove('active');
 });
