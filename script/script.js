970cab7cda77e22872c91b7836c15de002574d88
RohanAlief <rafarafa92653@gmail.com>
2025-09-21 07:49:17 +0700

    new feature test

--- script/script.js
+++ script/script.js
@@ -1,6 +1,15 @@
 const menuLst = document.getElementById('hamburger-menu');
 const menuToggle = document.getElementById('menu-toggle');
+const overlay = document.querySelector('.overlay');
 
+//menu toggle
 menuToggle.addEventListener('click', () => {
-    menuLst.classList.contains('active') ? menuLst.classList.remove('active') : menuLst.classList.add('active');
+  menuLst.classList.toggle('active');
+  overlay.classList.toggle('active');
+});
+
+// overlay
+overlay.addEventListener('click', () => {
+    menuLst.classList.remove('active');
+    overlay.classList.remove('active');
 });
